31a8371701e03c0a72b497d3a2db39bdea86c32f
Jędrzej Matuszewski <matuszewski99@gmail.com>
2020-03-17 20:32:16 +0100

    add woocommerce support

---
+++ woocommerce.php
@@ -0,0 +1,12 @@
+<?php get_header('page'); ?>
+
+<div class="main-container container">
+    <div class="row">
+        <div class="col-lg-12 mx-auto">
+            <?php woocommerce_content(); ?>
+        </div>
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
