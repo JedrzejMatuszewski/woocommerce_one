b4ed2cf83a75b5d9d70b616deb6d0b5d16602370
Jędrzej Matuszewski <matuszewski99@gmail.com>
2020-08-19 00:04:05 +0200

    Updated templates for woocommerce 4.4.0

--- woocommerce/cart/cross-sells.php
+++ woocommerce/cart/cross-sells.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Cross-sells
  *
@@ -11,36 +12,41 @@
  * the readme will list any important changes.
  *
  * @see https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce/Templates
- * @version 3.0.0
+ * @package WooCommerce\Templates
+ * @version 4.4.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-if ( $cross_sells ) : ?>
+if ($cross_sells) : ?>
 
 	<div class="cross-sells">
+		<?php
+		$heading = apply_filters('woocommerce_product_cross_sells_products_heading', __('You may be interested in&hellip;', 'woocommerce'));
 
-		<h2><?php esc_html_e( 'You may be interested in&hellip;', 'woocommerce' ); ?></h2>
+		if ($heading) :
+		?>
+			<h2><?php echo esc_html($heading); ?></h2>
+		<?php endif; ?>
 
 		<?php woocommerce_product_loop_start(); ?>
 
-			<?php foreach ( $cross_sells as $cross_sell ) : ?>
+		<?php foreach ($cross_sells as $cross_sell) : ?>
 
-				<?php
-					$post_object = get_post( $cross_sell->get_id() );
+			<?php
+			$post_object = get_post($cross_sell->get_id());
 
-					setup_postdata( $GLOBALS['post'] =& $post_object ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.OverrideProhibited, Squiz.PHP.DisallowMultipleAssignments.Found
+			setup_postdata($GLOBALS['post'] = &$post_object); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited, Squiz.PHP.DisallowMultipleAssignments.Found
 
-					wc_get_template_part( 'content', 'product' );
-				?>
+			wc_get_template_part('content', 'product');
+			?>
 
-			<?php endforeach; ?>
+		<?php endforeach; ?>
 
 		<?php woocommerce_product_loop_end(); ?>
 
 	</div>
-	<?php
+<?php
 endif;
 
 wp_reset_postdata();
